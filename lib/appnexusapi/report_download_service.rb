5521db1e724dedf091657002b3f25a4298cd96a5
Lincoln Lee <git@lincoln.hk>
2016-07-05 14:56:17 +0800

    Parse csv from report download body

--- lib/appnexusapi/report_download_service.rb
+++ lib/appnexusapi/report_download_service.rb
@@ -1,6 +1,7 @@
 class AppnexusApi::ReportDownloadService < AppnexusApi::Service
   def get(params={})
+    require 'csv'
     return_response = params.delete(:return_response) || false
-    response = CSV.parse(@connection.get(uri_suffix, {}).body)
+    response = ::CSV.parse(@connection.get(uri_suffix, params).body)
   end
 end
