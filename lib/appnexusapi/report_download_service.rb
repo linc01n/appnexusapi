828741f339c74077c625b0af381d5a18e8146eed
Lincoln Lee <git@lincoln.hk>
2016-07-27 18:16:16 +0800

    Download report file

--- lib/appnexusapi/report_download_service.rb
+++ lib/appnexusapi/report_download_service.rb
@@ -1,7 +1,12 @@
 class AppnexusApi::ReportDownloadService < AppnexusApi::Service
   def get(params={})
-    require 'csv'
     return_response = params.delete(:return_response) || false
-    response = ::CSV.parse(@connection.get(uri_suffix, params).body)
+    params = {
+      filename: "report_#{Time.now.strftime("%Y-%m-%d-%H-%M")}.csv"
+    }.merge(params)
+
+    # Download CSV file
+    File.open(params[:filename], "wb"){ |fp| fp.write(@connection.get(uri_suffix, params).body)}
+    response = @connection.get(uri_suffix, params)
   end
 end
